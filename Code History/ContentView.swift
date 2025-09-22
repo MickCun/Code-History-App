da4b72dac5275bb64fc99c141c67095684fa1c77
Michael Elliot Cunningham <michael.cunningham4@ucdconnect.ie>
2025-09-22 11:47:27 +0100

    Finished the question screen.

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -26,11 +26,54 @@ struct ContentView: View {
                     .multilineTextAlignment(.leading)
                 Spacer()
                 HStack {
+//                  Button 1
                     Button(action: {
                         print("Tapped on Choice 1")
                     }, label: {
-                        Text("Button")
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
                     })
+
+//                  Button 2
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    
+//                  Button 3
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    
+//                  Button 4
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width: 4)
+                    })
+                    
                 }
             }
         }
